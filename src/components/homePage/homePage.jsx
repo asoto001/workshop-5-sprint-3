ea89dc6baf5d14031d6bfda0770406c38529afcf
alejandro <soto.millan.ceni@gmail.com>
2023-03-04 10:52:04 -0500

    actualizacion del repo

---
+++ src/components/homePage/homePage.jsx
@@ -0,0 +1,11 @@
+import React from "react"; 
+import './styel.scss'
+
+
+const HomePage = () => {
+  <>
+  
+  </>
+}
+
+export default HomePage
---
+++ src/components/paySec/paySec.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './style.scss'
+
+const PaySec = () => {
+  <>
+  
+  </>
+}
+
+export default PaySec
